4dd09f9cd8e9ea4772166441c98c3a33679d3163
Jayden Andrews <jaydenrandrews@gmail.com>
2026-02-25 13:46:58 -0500

    Implemented abstract bank account

--- src/main/java/org/codedifferently/BankAccount.java
+++ src/main/java/org/codedifferently/BankAccount.java
@@ -1,4 +1,12 @@
 package org.codedifferently;
 
-public class BankAccount {
+public abstract class BankAccount {
+    private String owner;
+    private String accountNum;
+    private double balance;
+
+    public abstract void depositFunds();
+    public abstract void withdrawFunds();
+    public abstract void getMonthlyUpdate();
+    public abstract void printSummary();
 }
